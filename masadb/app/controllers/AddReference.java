85635cd38f0a6a6a9fbbe073261be58bc8c46230
Mikko Korhonen <korhmikko@gmail.com>
2013-04-18 09:31:03 +0300

    Sub-Millennium Bug Fix

--- masadb/app/controllers/AddReference.java
+++ masadb/app/controllers/AddReference.java
@@ -168,7 +168,11 @@ public class AddReference extends Controller {
             viite = viite.substring(0,4);
 
         // Vuosiluvun kaksi viimeista numeroa viitteen loppuun.
-        viite += Integer.toString(year).substring(2,4);
+		String yearString = Integer.toString(year);
+		if (yearString.length() < 2)
+			viite += yearString;
+		else
+			viite += yearString.substring(yearString.length()-2, yearString.length());
 
         return makeUniqueCiteKey(viite);
     }
